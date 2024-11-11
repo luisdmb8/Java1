5ee967a6a10652693b3bd603478ed566811dd474
ldavid <luisdmb8@gmail.com>
2024-11-11 10:05:46 -0500

    New Class

---
+++ HolaMundo/src/operadores/OperadorAnd.java
@@ -0,0 +1,12 @@
+package operadores;
+
+public class OperadorAnd {
+    public static void main(String[] args) {
+        System.out.println("***Operador and");
+        boolean a = true , b = false;
+        //and(regresa true si ambos valores son true)
+        var resultado = a && b;
+        System.out.println("resultado = " + resultado);
+    }
+
+}
