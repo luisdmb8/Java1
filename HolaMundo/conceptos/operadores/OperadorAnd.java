fd5de712481a9aa40abb8120b700d6e543c34e60
ldavid <luisdmb8@gmail.com>
2024-11-11 10:12:56 -0500

    Changes package's name

--- HolaMundo/conceptos/operadores/OperadorAnd.java
+++ HolaMundo/conceptos/operadores/OperadorAnd.java
@@ -2,7 +2,7 @@ package operadores;
 
 public class OperadorAnd {
     public static void main(String[] args) {
-        System.out.println("***Operador and");
+        System.out.println("***Operador and ***");
         boolean a = true , b = false;
         //and(regresa true si ambos valores son true)
         var resultado = a && b;
---
+++ HolaMundo/conceptos/operadores/OperadorOr.java
@@ -0,0 +1,10 @@
+package operadores;
+
+public class OperadorOr {
+    public static void main(String[] args) {
+        System.out.println("***Operador Or ***");
+        boolean a = true, b=false;
+        var resultado = a || b;
+        System.out.println("resultado = " + resultado );
+    }
+}
